64dfb8040bdba1fa33fd0804987a3bb95bd6826d
Elsa Cheng <jasloveel@gmail.com>
2018-07-09 12:13:59 +0800

    180709 - 初期環境整理

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require("gulp-autoprefixer");
 var autoprefixerOptions = {
-  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+  browsers: ["last 2 versions", "> 5%", "Firefox ESR"]
 };
-var slim = require('gulp-slim');
-var browserSync = require('browser-sync');
+var slim = require("gulp-slim");
+var browserSync = require("browser-sync");
 //var reload = browserSync.reload;
-
 
 var paths = {
   html: {
@@ -23,31 +22,34 @@ var paths = {
   }
 };
 
-
-gulp.task('slim', function () {
-  return gulp.src(paths.html.input)
-    .pipe(slim({
-      pretty: true,
-      options: "encoding='utf-8'",
-      require: 'slim/include',
-      format: 'xhtml',
-      options: 'include_dirs=["src/slim/includes/"]'
-    }))
+gulp.task("slim", function() {
+  return gulp
+    .src(paths.html.input)
+    .pipe(
+      slim({
+        pretty: true,
+        options: "encoding='utf-8'",
+        require: "slim/include",
+        format: "xhtml",
+        options: 'include_dirs=["src/slim/include/"]'
+      })
+    )
     .pipe(gulp.dest(paths.html.output));
 });
 
-gulp.task('sass', function () {
- return gulp.src(paths.sass.input)
-  .pipe(sourcemaps.init())
-  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-  .pipe(autoprefixer(autoprefixerOptions))
-  .pipe(sourcemaps.write(''))
-  .pipe(gulp.dest(paths.sass.output));
+gulp.task("sass", function() {
+  return gulp
+    .src(paths.sass.input)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(sourcemaps.write(""))
+    .pipe(gulp.dest(paths.sass.output));
 });
 
 /* Prepare Browser-sync for localhost */
-gulp.task('browser-sync', function () {
-  browserSync.init(['dist/js/*.js', 'dist/css/*.css', 'dist/*.html'], {
+gulp.task("browser-sync", function() {
+  browserSync.init(["dist/js/*.js", "dist/css/*.css", "dist/*.html"], {
     /*
     I like to use a vhost, WAMP guide: https://www.kristengrote.com/blog/articles/how-to-set-up-virtual-hosts-using-wamp, XAMP guide: http://sawmac.com/xampp/virtualhosts/
     */
@@ -55,20 +57,16 @@ gulp.task('browser-sync', function () {
     /* For a static server you would use this: */
 
     server: {
-      baseDir: './dist'
+      baseDir: "./dist"
     }
-
   });
 });
 
-
 // Watch scss, run the sass task on change.
-gulp.watch(paths.sass.input, gulp.series('sass'));
+gulp.watch(paths.sass.input, gulp.series("sass"));
 // Watch .html files, run the bs-reload task on change.
-gulp.watch(paths.html.input, gulp.series('slim'));
+gulp.watch(paths.html.input, gulp.series("slim"));
 
-gulp.task('build-dev', gulp.parallel('sass', 'slim'));
+gulp.task("build-dev", gulp.parallel("sass", "slim"));
 
-
-gulp.task('default',
-  gulp.series('build-dev', gulp.parallel('browser-sync')));
+gulp.task("default", gulp.series("build-dev", gulp.parallel("browser-sync")));
